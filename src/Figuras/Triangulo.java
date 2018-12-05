99df9b2f154803d7403338d6268ae947947ae12c
Javier Plaza Sisqués <javierplazasisque@gmail.com>
2018-12-05 11:09:20 +0100

    Triangulo

---
+++ src/Figuras/Triangulo.java
@@ -0,0 +1,19 @@
+package Figuras;
+import java.util.Scanner;
+
+public class Triangulo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int x = scan.nextInt();
+
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("😂");
+			}
+			System.out.println("😂");
+		}
+	}
+
+}
